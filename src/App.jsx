eded596629da83224f2d585995e113d3edcf9e16
Ahmed Gemechu <ahmedgemechu14@gmail.com>
2023-11-06 12:44:50 +0300

    i added componetn

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellowgreen;
+const StyledApp = styled.div`
+  padding: 20px;
 `;
-const Button = styled.button`
-  font-size: 1.2rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-`;
-
 function App() {
   return (
-    <div>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert("")}>Check in</Button>
-      <Button onClick={() => alert("")}>Check out</Button>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading>The Wild Oasis</Heading>
+        <Button onClick={() => alert("")}>Check in</Button>
+        <Button onClick={() => alert("")}>Check out</Button>
+        <Heading type="h2">Check in and out</Heading>
+        <Input type="number" placeholder="Number of guestes"></Input>
+        <Heading type="h3">Form</Heading>
+      </StyledApp>
+    </>
   );
 }
 
