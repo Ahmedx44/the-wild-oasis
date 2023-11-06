5ce60e128afff160653fc800221be65a687d7a19
Ahmed Gemechu <ahmedgemechu14@gmail.com>
2023-11-06 09:26:30 +0300

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellowgreen;
+`;
+const Button = styled.button`
+  font-size: 1.2rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <H1>The Wild Oasis</H1>
+      <Button onClick={() => alert(")}>Check in</Button>
+    </div>
+  );
 }
 
 export default App;
