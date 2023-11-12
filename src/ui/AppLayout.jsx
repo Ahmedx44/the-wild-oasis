de8d04385fb2770d2b59e8b30e8aed210e9a4da0
Ahmed Gemechu <ahmedgemechu14@gmail.com>
2023-11-12 09:48:37 +0300

    asd

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import Sidebar from "./SIdebar";
 import Header from "./Header";
+
+const Main = styled.main`
+  background-color: green;
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
     <div>
       <Header />
       <Sidebar />
-      <Outlet />
+      <Main>
+        <Outlet />
+      </Main>
     </div>
   );
 }
