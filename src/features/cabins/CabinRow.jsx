88bb47a1fb65810e4c40fb1d88f6182cd32d18f7
Ahmed Gemechu <ahmedgemechu14@gmail.com>
2023-11-20 23:53:19 +0300

    installed package for foom hook and Toaster  and add fucntionlly of notifiing withing toaster

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -55,13 +56,16 @@ function CabinRow({ cabin }) {
   const { mutate, isLoading } = useMutation({
     mutationFn: (id) => deleteCabin(id),
     onSuccess: () => {
-      alert("Cabin succesfully deleted");
+      toast.success("Cabin successfully deleted");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
-      onerror: (err) => alert(err.message);
+    },
+    onError: (err) => {
+      toast.error(err.message);
     },
   });
+
   return (
     <TableRow>
       <Img src={image} />
