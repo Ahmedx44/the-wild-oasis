365b68247db9e0c3524d2796417665924c6f20da
Ahmed Gemechu <Ahmedgemechu14@gmail.com>
2023-12-21 00:33:33 +0300

    mak the table body reusabale

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,15 +4,9 @@ import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
+import Table from "../../ui/Table";
 
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
 
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
 
 const TableHeader = styled.header`
   display: grid;
@@ -33,18 +27,19 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
   return (
-    <Table role="table">
-      <TableHeader role="row">
+    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+      <Table.Header role="row">
         <div></div>
         <div>Cabin</div>
         <div>Capcity</div>
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </TableHeader>
-      {cabin.map((cabin) => (
+
+      </Table.Header>
+      <Table.Body data={cabin} render={(cabin) => (
         <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
+      )}/>
     </Table>
   );
 }
